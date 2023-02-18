528bd7ab452be1909a4bbb10af7d7bc7128c9f08
Anneliese Kappey <108365625+anneliesekappey@users.noreply.github.com>
2023-02-18 15:15:22 +0100

    more fixes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { Home } from './pages/Home.js'
 import { AboutTheAuthor } from './pages/AboutTheAuthor.js'
 import { Contact } from './pages/Contact.js'
 import { AboutTheBook } from './pages/AboutTheBook.js'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/abouttheauthor" element={<AboutTheAuthor />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/aboutthebook" element={<AboutTheBook />} />
+        <Route path="/" element=<Home /> />
+        <Route path="/abouttheauthor" element=<AboutTheAuthor /> />
+        <Route path="/contact" element=<Contact /> />
+        <Route path="/aboutthebook" element=<AboutTheBook /> />
       </Routes>
     </div>
   )
